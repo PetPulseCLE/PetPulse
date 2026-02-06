0c0adab6d553cb7807656f7c983d3446eb3dd8a4
Will <hyndswill@gmail.com>
2026-02-06 10:22:03 -0500

    Remove ble connection testing from index.tsx

--- petpulse-mobile/app/(tabs)/index.tsx
+++ petpulse-mobile/app/(tabs)/index.tsx
@@ -1,10 +1,5 @@
 import { View } from "react-native";
-import BleManager from "react-native-ble-manager";
-import { useEffect } from "react";
 
 export default function Index() {
-  useEffect(() => {
-    BleManager.start({showAlert: true});
-  }, []);
   return <View />;
 }
