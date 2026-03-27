4b3284c7121d38adc93c68b3c15b64c5a1db31e8
Renad Nofal <renad.nofal126@gmail.com>
2026-03-27 11:54:02 -0400

    minor fixes

--- petpulse-mobile/app/(tabs)/index.tsx
+++ petpulse-mobile/app/(tabs)/index.tsx
@@ -272,24 +272,30 @@ export default function Index() {
             className="flex flex-row gap-2 px-2 min-w-0"
             entering={FadeIn.delay(300).duration(500).easing(Easing.inOut(Easing.ease))}
           >
-            <Card className="bg-tab-bar border-tab-bar shadow-sm basis-1/2">
-              <CardHeader>
-                <View className="flex flex-row items-center justify-between">
-                  <View className="flex flex-row items-center gap-1">
-                    <Icon as={Thermometer} size={22} className="text-amber-500" />
-                    <Text className="text-md font-semibold text-secondary-foreground">Temperature</Text>
+            <Pressable
+              className="basis-1/2 shadow-sm active:scale-95 transition-all duration-300"
+              onPress={() => router.push('/temperature')}
+            >
+              <Card className="bg-tab-bar border-tab-bar">
+                <CardHeader>
+                  <View className="flex flex-row items-center justify-between">
+                    <View className="flex flex-row items-center gap-1">
+                      <Icon as={Thermometer} size={22} className="text-amber-500" />
+                      <Text className="text-md font-semibold text-secondary-foreground">Temperature</Text>
+                    </View>
+                    <Icon as={ChevronRight} className="size-4 text-muted-foreground" />
                   </View>
-                </View>
-                <CardDescription>
-                  <Text className="text-muted-foreground text-sm">
-                    {lastEnvRef.current?.temperature}°F, {getTime(envLastUpdatedRef.current)}
-                  </Text>
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <Text className="text-muted-foreground text-sm">{env?.temperature}</Text>
-              </CardContent>
-            </Card>
+                  <CardDescription>
+                    <Text className="text-muted-foreground text-sm">
+                      {lastEnvRef.current?.temperature}°F, {getTime(envLastUpdatedRef.current)}
+                    </Text>
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <Text className="text-muted-foreground text-sm">{env?.temperature}</Text>
+                </CardContent>
+              </Card>
+            </Pressable>
             <Pressable
               className="basis-1/2 shadow-sm active:scale-95 transition-all duration-300"
               onPress={() => router.push('/humidity')}
